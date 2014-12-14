40ed524d1e8f34fe508432cb89d91ebb2d10dfb6
Gea-Suan Lin <gslin@gslin.org>
2014-12-15 03:23:26 +0800

    Use "debuild clean" to cleanup.

--- upload.sh
+++ upload.sh
@@ -2,8 +2,11 @@
 
 CODENAMES="precise trusty"
 
+debuild clean
+
 for CODENAME in $CODENAMES; do
     sed "s/%%CODENAME%%/${CODENAME}/" debian/changelog.template > debian/changelog
     debuild -S -sa
     dput ppa:gslin/icdiff ../icdiff_*.changes
+    debuild clean
 done
