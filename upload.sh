3cac768ceba7994aa152b94cd4fd0cfffa87c26c
Gea-Suan Lin <gslin@gslin.org>
2014-12-15 03:33:06 +0800

    Use -f to force upload.

--- upload.sh
+++ upload.sh
@@ -8,7 +8,7 @@ rm ../icdiff_*
 for CODENAME in $CODENAMES; do
     sed "s/%%CODENAME%%/${CODENAME}/" debian/changelog.template > debian/changelog
     debuild -S -sa
-    dput ppa:gslin/icdiff ../icdiff_*.changes
+    dput -f ppa:gslin/icdiff ../icdiff_*.changes
     debuild clean
     rm ../icdiff_*
 done
