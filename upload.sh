ba2a4155d80c50ca45a3a2dcab172931e265bd23
Gea-Suan Lin <gslin@gslin.org>
2014-12-15 03:25:54 +0800

    Remove icdiff_* before/after dput.

--- upload.sh
+++ upload.sh
@@ -3,10 +3,12 @@
 CODENAMES="precise trusty"
 
 debuild clean
+rm ../icdiff_*
 
 for CODENAME in $CODENAMES; do
     sed "s/%%CODENAME%%/${CODENAME}/" debian/changelog.template > debian/changelog
     debuild -S -sa
     dput ppa:gslin/icdiff ../icdiff_*.changes
     debuild clean
+    rm ../icdiff_*
 done
